480ff66894307263882cf124e9e7ae37f9ee196f
young <syoh0313@yonsei.ac.kr>
2023-12-09 15:37:42 +0900

    feat : coupon edit

--- src/view/AdminCoupon.vue
+++ src/view/AdminCoupon.vue
@@ -31,18 +31,40 @@
               <td>{{ item.discountPercent }}</td>
               <td>{{ item.createdAt }}</td>
               <td>{{ item.status }}</td>
-              <td>편집</td>
+              <td @click="openModel(item)" id="edit">편집</td>
               <td @click="deleteCoupon(item.id)" id="delete">삭제</td>
             
             </tr>
           </table>
         </div>
-
-
     </div>
 
 
+    <!-- 설문 수정 모달창 -->
+      <div v-if="editModal == true" class="edit-modal">
+        <div class="edit-contentsbox">
+          <div id="edit-top">
+            <a class="edit-close" @click="closeModal">X</a>
+            <p class="edit-title">쿠폰 수정하기</p>
 
+          </div>
+      
+          <div id="edit-container">
+            <div id="detail-title">쿠폰 코드</div>
+              <input class="modal-input" type="text" v-model="editInfo.code">
+            
+            <div id="detail-title">쿠폰 rate</div>
+              <input class="modal-input" type="text" v-model="editInfo.discountPercent">
+            
+            <div id="detail-title">쿠폰 상태</div>
+                <select class="modal-input" v-model="editInfo.status" aria-label="Default select example">
+                  <option v-for="item in statusType" :key="item.key" :value=item>{{ item }}</option>
+                </select>
+            
+            <button id="edit-fin-btn" @click="editCoupon">수정 완료</button>
+           </div> 
+      </div>
+    </div>
 
   </div>
 </template>
@@ -54,10 +76,20 @@ export default {
     return {
       couponInfo: {
         code: '',
-        rate: 0,
+        discountPercent: 0,
       },
 
+      editInfo: {
+        id : 0,
+        code : '',
+        discountPercent : 0,
+        status : ''
+      },
+      statusType: ["VALID", "INVALID"],
       couponList: [],
+
+      editModal : false,
+
 
     }
   },
@@ -68,9 +100,18 @@ export default {
 
   methods: {
 
+    closeModal() {
+      this.editModal = false
+    },
+
+    openModel(item){
+      this.editModal = true
+      this.editInfo = item
+    },
+
     async getCouponList(){
       try {
-        const response = await axios.get("http://3.39.170.7/coupon")
+        const response = await axios.get("https://gosurveasy.co.kr/coupon")
         this.couponList = response.data.couponList
         console.log(response.data.surveyList)
       } catch (error) {
@@ -82,7 +123,7 @@ export default {
     async addCoupon(){
       try {
         await axios.post(
-          'http://3.39.170.7/coupon',
+          'https://gosurveasy.co.kr/coupon',
           {
             code : this.couponInfo.code.toString(),
             discountPercent : this.couponInfo.rate
@@ -97,7 +138,7 @@ export default {
     async deleteCoupon(id){
       try {
         if (confirm("정말 삭제하시겠습니까?")) {
-          const response = await axios.delete(`http://3.39.170.7/coupon/${id}`)
+          const response = await axios.delete(`https://gosurveasy.co.kr/coupon/${id}`)
           if (response.status == 200) {
             if (confirm("삭제되었습니다.")) {
               this.$router.go("/admincoupon")
@@ -113,19 +154,19 @@ export default {
       }
     },
 
-    async editSurvey() {
+    async editCoupon() {
       try {
+        console.log(this.editInfo)
         await axios.patch(
-          `http://3.39.170.7/survey/mypage/edit/${this.editTarget.id}`,
+          `https://gosurveasy.co.kr/coupon/${this.editInfo.id}`,
           {
-            title: this.modalTitle,
-            link: this.modalLink,
-            headCount: this.modalHeadCount,
-            price: this.updatePrice
+            code : this.editInfo.code,
+            discountPercent : this.editInfo.discountPercent,
+            status : this.editInfo.status
           }
         )
         this.editModal = false
-        this.$router.go("/mypage/order")
+        this.$router.go("/admincoupon")
       } catch (error) {
         console.log(error)
       }
@@ -244,4 +285,109 @@ export default {
 
 .cou-tds.group {
   color: rgb(69, 149, 235);
-}</style>
+}
+
+.edit-modal {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.202);
+  display: table;
+  transition: opacity .3s ease;
+}
+.edit-contentsbox {
+  display: flex;
+  flex-direction: column;
+  padding: 20px 40px;
+  font-family: 'Noto Sans KR', sans-serif;
+  width: 600px;
+  height: 560px;
+  margin: 120px auto;
+  padding-top: 15px;
+  padding-bottom: 30px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 20px;
+  box-shadow: 0 2px 3px rgba(56, 56, 56, 0.042);
+  transition: all .3s ease;
+  z-index: 2;
+}
+#edit-container-price {
+  text-align: right;
+}
+#edit-container-price-diff {
+  text-align: center;
+  color: #0AAB00;
+}
+#edit-container-price-after {
+  padding: 3px;
+  font-size: 20px;
+  text-align: center;
+  background: #0AAB00;
+  color: #FFFFFF;
+}
+.edit-title {
+  text-align: center;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 23px;
+  font-weight: bold;
+  margin-bottom: 9px;
+  color: #0CAE02;
+}
+.edit-close {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  cursor: pointer;
+  color:#494949;
+}
+#detail-title {
+  font-size: 16px;
+  color: #000000;
+  text-align: left;
+  margin: 5px;
+}
+#container-link-btn {
+  display: flex;
+  justify-content: right;
+}
+#link-btn {
+  border: 0;
+  font-size: 15px;
+  color: #0CAE02;
+}
+#edit-fin-btn {
+  padding: 7px;
+  margin: 15px 0px 0px 0px;
+  color:#0CAE02;
+  background-color: #FFFFFF;
+  border: 1.5px solid #0CAE02;
+  border-radius: 10px;
+  font-size: 0.9rem;
+  cursor: pointer;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+#edit-fin-btn:hover{
+  color: white;
+  background: #0AAB00;
+}
+
+.modal-input {
+  margin-top: 10px;
+  margin-bottom: 15px;
+  width: 98%;
+  height: 40px;
+  padding-left: 7px;
+  color: #a0a0a0;
+  background-color: white;
+  border: solid 1px #dadada;
+  border-radius: 5px;
+}
+
+#edit{
+  cursor: pointer;
+  color: #0AAB00;
+}
+</style>
